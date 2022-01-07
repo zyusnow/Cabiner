b1e01c4e10a3f4c90eab77d21218b06edcc86ecd
Zhen <zyuluna@gmail.com>
2022-01-07 10:40:13 -0800

    frontend

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import './Navigation.css';
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <>
+      <button onClick={openMenu}>
+        <img className="usericon" src="../../../user.png" onClick={openMenu}></img>
+      </button>
+      {showMenu && (
+        <ul className="profile-dropdown">
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>
+            <button onClick={logout}>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default ProfileButton;
