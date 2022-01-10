e23166a63be540d1388aab4dc25f26146e8bc03a
Zhen <zyuluna@gmail.com>
2022-01-10 10:53:51 -0800

    test

---
+++ backend/db/migrations/20220110184649-create-spot.js
@@ -0,0 +1,59 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      lat: {
+        type: Sequelize.DECIMAL
+      },
+      ing: {
+        type: Sequelize.DECIMAL
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      zipcode: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Spots');
+  }
+};
---
+++ backend/db/models/spot.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Spot = sequelize.define('Spot', {
+    userId: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    country: DataTypes.STRING,
+    lat: DataTypes.DECIMAL,
+    ing: DataTypes.DECIMAL,
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    zipcode: DataTypes.INTEGER,
+    description: DataTypes.STRING
+  }, {});
+  Spot.associate = function(models) {
+    // associations can be defined here
+  };
+  return Spot;
+};
