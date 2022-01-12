5c7f75d4ee3d0f69940fb609bbac9248ef084360
Zhen <zyuluna@gmail.com>
2022-01-12 02:47:22 -0800

    update

--- frontend/src/components/NewSpotFormModal/NewSpotForm.js
+++ frontend/src/components/NewSpotFormModal/NewSpotForm.js
@@ -10,7 +10,6 @@ import './NewSpotForm.css'
 function NewSpotForm() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const session = useSelector(state => state.session);
   const sessionUser = useSelector((state) => state.session.user);
 
   const userId = sessionUser.id;
@@ -27,14 +26,14 @@ function NewSpotForm() {
 
   // if (sessionUser) return <Redirect to="/" />;
   useEffect(() => {
-    const validationErrors = [];
+    const errors = [];
     if (address.length > 200) errors.push("Address must not be more than 200 characters long")
     if (city.length > 50) errors.push("City must not be more than 50 characters long")
     if (state.length > 50) errors.push("State must not be more than 50 characters long")
     if (country.length > 50) errors.push("Country must be less 50 characters")
     if (name.length > 50) errors.push("Name must not be more than 50 characters long")
     if (description.length > 1000) errors.push("Description must not be more than 1000 characters long")
-    if (price < 1) errors.push("Please provide a valid price")
+    if (price < 0) errors.push("Please provide a valid price")
     if (zipcode < 0) errors.push("Please provide a valid zip code")
     if (url.length > 250) errors.push("Please provide a image url.")
     setErrors(errors)
@@ -43,43 +42,46 @@ function NewSpotForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // const newSpot = dispatch(addNewSpot({
-    //     address,
-    //     city,
-    //     state,
-    //     country,
-    //     name,
-    //     price,
-    //     zipcode,
-    //     description,
-    //     userId,
-    //     url
-    // }))
-    // .catch(async (res) => {
-    //   const data = await res.json();
-    //   if (data && data.errors) setErrors(data.errors);
-    const newSpot = {
-      address,
-      city,
-      state,
-      country,
-      name,
-      price,
-      zipcode,
-      description,
-      userId,
-      url
-    }
-    let data;
-    try{
-      data = dispatch(addNewSpot(newSpot));
-    } catch(error) {
-      throw new Error
-    }
-    if (data) {
-      history.push(`/spots/${data.id}`);
-    }
-  }
+    const newSpot = dispatch(addNewSpot({
+        address,
+        city,
+        state,
+        country,
+        name,
+        price,
+        zipcode,
+        description,
+        userId,
+        url
+    }))
+    .catch(async (res) => {
+      const data = await res.json();
+      console.log(data)
+      if (data && data.errors) setErrors(data.errors);
+    })}
+  //   const newSpot = {
+  //     address,
+  //     city,
+  //     state,
+  //     country,
+  //     name,
+  //     price,
+  //     zipcode,
+  //     description,
+  //     userId,
+  //     url
+  //   }
+  //   let data;
+  //   try{
+  //     data = dispatch(addNewSpot(newSpot));
+  //   } catch(error) {
+  //     throw new Error
+  //   }
+  //   console.log("i'm data",data)
+  //   if (data) {
+  //     history.push(`/spots/${data.id}`);
+  //   }
+  // }
 
   return (
     <div className='login_container'>
