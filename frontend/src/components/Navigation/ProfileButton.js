95a5d04e153a52514ffc3c972fc9bb9c7bb69284
Zhen <zyuluna@gmail.com>
2022-01-13 00:31:56 -0800

    css fix

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -17,21 +17,21 @@ function ProfileButton({ user }) {
   const openMenu = (e) => {
     e.preventDefault();
     setShowMenu(!showMenu);
-    // if (showMenu) return;
-    // setShowMenu(true);
+    if (showMenu) return;
+    setShowMenu(true);
   };
 
-  // useEffect(() => {
-  //   if (!showMenu) return;
+  useEffect(() => {
+    if (!showMenu) return;
 
-  //   const closeMenu = () => {
-  //     setShowMenu(false);
-  //   };
+    const closeMenu = () => {
+        setShowMenu(false);
+    };
 
-  //   document.addEventListener('click', closeMenu);
+    document.addEventListener('click', closeMenu);
 
-  //   return () => document.removeEventListener("click", closeMenu);
-  // }, [showMenu]);
+    return () => document.removeEventListener("click", closeMenu);
+}, [showMenu]);
 
   const logout = (e) => {
     e.preventDefault();
